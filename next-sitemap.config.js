6944ee4f566668da0a535f7487514902ce1b7c34
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-03-17 02:15:12 +0700

    add google sg and robots sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://deepfacial.github.io',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+  // Transform function to customize each URL entry
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: path === '/' ? 1.0 : config.priority,
+      lastmod: new Date().toISOString(),
+    }
+  },
+} 
